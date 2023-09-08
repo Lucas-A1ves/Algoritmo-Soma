23024361f666c82937f31fa8178e171dafc2ee11
Lucas Alves <137112552+Lucas-A1ves@users.noreply.github.com>
2023-09-08 16:30:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+ Solicite 2 números, faça a soma deles e apresente o resultado final ao usuário.
+
+ Request two numbers, Sum them up and present the result to the end user
+ 
+*/
+
+ let numberOne= Number (prompt (" Enter the number below "))
+ let numberTwo= Number (prompt (" Enter the number below"))
+  
+ let result = numberOne + numberTwo
+
+ alert(" The result is " + result)
+
+   
